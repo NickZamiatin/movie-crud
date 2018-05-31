353ad5fcdf8edd4450f771260592c74b74bc1cae
Nikita Zamiatin <4406015@gmail.com>
2018-05-31 14:28:55 -0400

    basic

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const cors = require('cors');
+const movieRoutes = require('./routes/movies')
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.bodyParser({extended : true}));
+
+
+
+
+app.get('/', (request, response) => {
+    response.send('menu-knex')
+})
+
+app.use('/movies', movieRoutes);
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`library-express: Express application is listening on port ${process.env.PORT}...`);
+});
---
+++ migrations/20180531111751_create_table_movies.js
@@ -0,0 +1,9 @@
+// This is a dummy migration file created to repair a broken migration process. It will throw a warning because it doesn't return a promise.
+
+exports.up = function (knex, Promise) {
+    
+};
+
+exports.down = function (knex, Promise) {
+
+};
